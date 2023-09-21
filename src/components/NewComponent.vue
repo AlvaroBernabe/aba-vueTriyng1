557a5d1e4f66ba08eea09f8e4768f82706d075dd
alvarito101093 <alvaro101093@gmail.com>
2023-09-21 19:49:49 +0200

    feat: playing with interpolation

--- src/components/NewComponent.vue
+++ src/components/NewComponent.vue
@@ -1,15 +1,33 @@
 <template>
-    <h1>Hello</h1>
+    <h1>Hello{{ 10 + 10 }}</h1>
+    <h2>{{ "hello".length }}</h2>
+    <h2>email: {{ email }}</h2>
+    <h2>telefono: {{ mobile }}</h2>
+    <h2>nombre: {{ getName1 }}</h2>
+    <h2>nombre: {{ getName1() }}</h2>
 </template>
 
 <script>
 export default{
-    name:"PAGES"
-}
+    name:"PAGES",
+    // email: "ejemplo@email.com",
+    data(){
+        return {
+            email: "ejemplo@email.com",
+            mobile: "66644455587",
+            getName1: function() {
+                return "me lamo Alvaro";
+            },
+        };
+    },
+};
 </script>
 <style scoped>
 h1 {
     color: red;
+}
+h2 {
+    color: blue;
 }
 
 </style>
