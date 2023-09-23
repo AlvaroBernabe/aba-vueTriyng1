f807eafc4bbc7878d0e6455e1faf2f229c504f25
alvarito101093 <alvaro101093@gmail.com>
2023-09-23 20:49:56 +0200

    feat: playing with event/parameters

--- src/components/NewComponent.vue
+++ src/components/NewComponent.vue
@@ -7,20 +7,35 @@
     <h2>telefono: {{ mobile == 999 }}</h2>
     <h2>nombre: {{ getName1 }}</h2>
     <h2>nombre: {{ getName1() }}</h2>
+    <h2>{{getData()}}</h2>
 </template>
 
 <script>
-export default{
-    name:"PAGES",
+export default {
+    name: "PAGES",
     // email: "ejemplo@email.com",
-    data(){
+    data() {
         return {
             email: "ejemplo@email.com",
             mobile: "66644455587",
-            getName1: function() {
-                return "me lamo Alvaro";
-            },
+            // getName1: function() {
+            //     return "me lamo Alvaro";
+            // },
         };
+    },
+    methods: {
+        getName1: function () {
+            return "me lamo Alvaro";
+        },
+        getName(Name) {
+            return Name;
+        },
+        getData() {
+            return {
+                email: this.email,
+                mobile: this.mobile
+            };
+        },
     },
 };
 </script>
@@ -28,8 +43,8 @@ export default{
 h1 {
     color: red;
 }
+
 h2 {
     color: blue;
 }
-
 </style>
