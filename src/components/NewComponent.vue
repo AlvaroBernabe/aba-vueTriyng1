b7454a87054e3aea14802b3155a5ed4015c7b425
alvarito101093 <alvaro101093@gmail.com>
2023-09-23 23:52:19 +0200

    feat: playing with event/parameters

--- src/components/NewComponent.vue
+++ src/components/NewComponent.vue
@@ -4,10 +4,16 @@
     <h2>email: {{ email }}</h2>
     <h2>telefono: {{ mobile }}</h2>
     <!-- <h2>telefono: {{ mobile = 999 }}</h2> -->
-    <h2>telefono: {{ mobile == 999 }}</h2>
+    <!-- <h2>telefono: {{ mobile == 999 }}</h2>
     <h2>nombre: {{ getName1 }}</h2>
-    <h2>nombre: {{ getName1() }}</h2>
-    <h2>{{getData()}}</h2>
+    <h2>nombre: {{ getName1() }}</h2> -->
+    <!-- <h2>{{getData()}}</h2> -->
+    <button v-on:click="ejemplo1()">Boton 1</button>
+    <button v-on:click="ejemplo2()">Boton 2</button>
+    <button v-on:dblclick="ejemplo3()">Boton Doble click</button>
+    <!-- <h1 v-on:mousemove="getConsole('')">Home Page</h1> Esto funciona cuando el ratón este encima -->
+    <h1 v-on:mousemove="getConsole('')">Home Page</h1> 
+
 </template>
 
 <script>
@@ -21,6 +27,7 @@ export default {
             // getName1: function() {
             //     return "me lamo Alvaro";
             // },
+            ejemplo1() {console.log("Has pulsado el boton correctamente")},
         };
     },
     methods: {
@@ -36,6 +43,18 @@ export default {
                 mobile: this.mobile
             };
         },
+        ejemplo2() {
+            alert("Has pulsado el Botón II")
+        },
+        ejemplo3() {
+            alert("Has pulsado el Botón 3")
+        },
+        // getConsole() {
+        //     console.error("Function is running")
+        // },
+        getConsole() {
+            console.warn("Function is running")
+        }
     },
 };
 </script>
