98c8ac9a7166e5ab561912b4ba71c54522f6d663
alvarito101093 <alvaro101093@gmail.com>
2023-09-24 00:37:07 +0200

    feat:  multiple binding

--- src/components/inputboxes.vue
+++ src/components/inputboxes.vue
@@ -2,16 +2,28 @@
     <h1>How to add input boxes in multiple binding</h1>
     <label for="cricket">Cricket</label>
     &nbsp;
-    <input type="checkbox">
+    <input type="checkbox" value="Cricket" v-model="sport">
     <br />
-    <label for="bootball">Football</label>
+    <label for="football">Football</label>
     &nbsp;
-    <input type="checkbox">
+    <input type="checkbox" value="Football" v-model="sport">
     <br />
     <label for="hockey">Hockey</label>
     &nbsp;
-    <input type="checkbox">
-    <h3>Selected Sport</h3>
+    <input type="checkbox" value="Hockey" v-model="sport">
+    <br>
+    <h3>Selected Sport: {{ sport }}</h3>
+    <h3>Selected Player: {{ player }}</h3>
+
+    <label for="playerName">Player Name:</label>
+    &nbsp; &nbsp;
+
+    <label for="">Smith</label>
+    <input type="radio" value="Smith" name="name" id="name" v-model="player">
+
+    <label for="">Fich</label>
+    <input type="radio" value="Fich" name="name" id="name2" v-model="player">
+    <br>
 </template>
 
 <script>
@@ -19,7 +31,8 @@ export default {
     name: "inputBoxes",
     data() {
         return{
-            count: 20
+            sport: [],
+            player: []
     }}
 }
 </script>
