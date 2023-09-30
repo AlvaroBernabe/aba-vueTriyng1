79a56d0d09794d15d904bffc897127d9d31d8c7c
alvarito101093 <alvaro101093@gmail.com>
2023-09-30 13:54:00 +0200

    feat: color swap

--- src/components/child-components.vue
+++ src/components/child-components.vue
@@ -1,17 +1,24 @@
 <template>
-    <ul>
-        <li v-for="item in users" :key="item.name">
-            <NewUser :data="item" />
-        </li>
+    <!-- <h1>child components</h1>
+    <h3>{{ name }}</h3>
+    <h3>{{ user.name }}</h3>
+    <button v-on:click="getData()">Call the Function</button> -->
+    <!-- <ul>
+      <li v-for="item in users" :key="item">
+        <Newuser :data="item" />
+      </li>
     </ul>
-    <div v-html="emailadresss"></div>
-    <h1 v-html="id"></h1>
-
+    <div>
+      <h1 v-html="emailadress"></h1>
+      <h1 v-html="id"></h1>
+    </div> -->
+    <h1>Class Binding</h1>
+    <h3 :class="{ yellow: color }">Dynamic class</h3>
+    <button v-on:click="color = !color">change the color</button>
     <!-- <NewUser /> -->
-</template>
-
-<script>
-import NewUser from "./new-user.vue";
+  </template>
+  <script>
+// import NewUser from "./new-user.vue";
 export default {
     name: "child-components",
     props: {
@@ -20,40 +27,45 @@ export default {
         getData: Function
     },
     data() {
-        return {
-            // users: [
-            //     {
-            //         name: "jacson",
-            //         email: "jackson@email.com",
-            //     },
-            //     {
-            //         name: "juan",
-            //         email: "juan@email.com",
-            //     },
-            //     {
-            //         name: "paco",
-            //         email: "paco@email.com",
-            //     },
-            //     {
-            //         name: "federico",
-            //         email: "federico@email.com",
-            //     },
-            // ]
-            anmes: "peter",
-            emailadresss: "<h1>peter@gmail.com</h1>",
-            id: 895641513,
-        }
+      return {
+        // users: [
+        //   {
+        //     name: "jackson",
+        //     email: "jackson454@gmail.com",
+        //   },
+        //   {
+        //     name: "jamws",
+        //     email: "james4564@gmail.com",
+        //   },
+        //   {
+        //     name: "Henry",
+        //     email: "Henry@gmail.com",
+        //   },
+        //   {
+        //     name: "Oliver",
+        //     email: "Oliver788@gmail.com",
+        //   },
+        //   {
+        //     name: "Tony",
+        //     email: "Tony3004@gmail.com",
+        //   },
+        // ],
+        // names: "peter",
+        // emailadress: "<h3>peter123@gmail.com<h3/>",
+        // id: "<h3> 16465466 <h3/>",
+        color: false,
+      };
     },
-    components: { NewUser },
-};
-</script>
-
-<style>
-h1 {
-    color: red;
-}
-h3 {
+    // components: { NewUser },
+  };
+  </script>
+  <style>
+  .yellow {
     color: blue;
     font-weight: bold;
-}
-</style>
+    background-color: yellow;
+    width: 150px;
+    height: 50px;
+  }
+  </style>
+  
